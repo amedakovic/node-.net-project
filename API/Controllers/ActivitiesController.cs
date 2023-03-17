31d16646f29d207a5913e381c592e791056bbdd6
amedakovic <medakovic.aleks1@gmail.com>
2023-03-17 20:35:14 +0100

    added react semantic ui, loading data from the api

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetAcitvities()
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await _context.Activities.ToListAsync();    
         }

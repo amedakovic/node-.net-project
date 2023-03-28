79995d7bbe3eaa2de2e12d0ad945678e487b6f2f
amedakovic <medakovic.aleks1@gmail.com>
2023-03-28 17:31:01 +0200

    changed form using formik

---
+++ client-app/src/app/common/options/categoryOptions.tsx
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Film', value: 'film'},
+    {text: 'Food', value: 'food'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Musik', value: 'musik'},
+    {text: 'Travel', value: 'travel'}
+]

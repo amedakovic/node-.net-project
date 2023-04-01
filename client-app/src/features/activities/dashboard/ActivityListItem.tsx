92ffbae77d09e3a7af10fe02dd1a7fd8c6d4accd
amedakovic <medakovic.aleks1@gmail.com>
2023-04-01 18:35:36 +0200

    attending to frontend

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,9 +1,10 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Button, Icon, Item, ItemDescription, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import {format} from 'date-fns';
+import ActivityListItemAttendee from './ActivityListItemAttendee';
 
 interface Props {
     activity: Activity
@@ -12,26 +13,33 @@ interface Props {
 export default function ActivityListItem({activity}: Props){
 
     const {activityStore} = useStore();
-    const {deleteActivity, loading} = activityStore;
-    const[target, setTarge] = useState('');
+    const {deleteActivity} = activityStore;
 
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id:string){
-        setTarge(e.currentTarget.name);
-        deleteActivity(id);
-    }
 
 
     return(
         <Segment.Group>
             <Segment>
+                {activity.isCancelled && 
+                    <Label attached='top' color='red' content='Cancelled' style={{textAlign: 'center'}}/>}
                 <Item.Group>
                     <Item>
-                        <Item.Image size='tiny' circular src='assets/user.png'/>
+                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src='assets/user.png'/>
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description>Hosted by</Item.Description>
+                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+                            {activity.isHost && (
+                                <ItemDescription>
+                                    <Label basic color='orange'>You are hosting this activity</Label>
+                                </ItemDescription>
+                            )}
+                            {activity.isGoing && !activity.isHost && (
+                                <ItemDescription>
+                                    <Label basic color='green'>You are going to this activity</Label>
+                                </ItemDescription>
+                            )}
                         </Item.Content>
                     </Item>
                 </Item.Group>
@@ -43,7 +51,7 @@ export default function ActivityListItem({activity}: Props){
                 </span>
             </Segment>
             <Segment secondary>
-                Attendees go here
+                <ActivityListItemAttendee attendees={activity.attendees!}/>
             </Segment>
             <Segment clearing>
                 <span>{activity.description}</span>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button,Header,Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layouts/LoadingComponent";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 import { Formik, Form } from "formik";
 import * as Yup from 'yup'; 
@@ -24,15 +24,7 @@ export default observer(function ActivityForm(){
     const {id} = useParams();
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState<Activity>({
-        id:'',
-        title:'',
-        category:'',
-        description:'',
-        date: null,
-        city:'',
-        venue:''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -44,13 +36,17 @@ export default observer(function ActivityForm(){
     })
 
     useEffect(()=> {
-        if(id)loadActivity(id).then(activity => setActivity(activity!));
+        if(id)loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     }, [id, loadActivity])
 
-   function handleFormSubmit(activity: Activity){
+   function handleFormSubmit(activity: ActivityFormValues){
         if(!activity.id){
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            }
             activity.id = uuid();
-            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`));
         }
         else{
             updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
@@ -78,7 +74,7 @@ export default observer(function ActivityForm(){
                         <Header content='Location Details' sub color="teal" />
                         <MyTextInput placeholder='City' name='city'/>
                         <MyTextInput placeholder='Venu' name='venue'/>
-                        <Button disabled={isSubmitting || !dirty || !isValid} loading={loading} floated="right" positive type="submit" content='Submit'/>
+                        <Button disabled={isSubmitting || !dirty || !isValid} loading={isSubmitting} floated="right" positive type="submit" content='Submit'/>
                         <Button as={Link} to='/activities' floated="right" type="submit" content='Cancel'/>
                     </Form>
                   )}

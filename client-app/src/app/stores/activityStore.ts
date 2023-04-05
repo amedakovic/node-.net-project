d43eda527d766dc0b7a6beeb4218a9ccb280258b
amedakovic <medakovic.aleks1@gmail.com>
2023-04-05 18:22:05 +0200

    added comments, using signalR

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -181,5 +181,9 @@ export default class ActivityStore{
         runInAction(() => this.loading = false)
     }
    }
+
+   clearSelectedActivity = () => {
+    this.selectedActivity = undefined;
+   }
  }
 
